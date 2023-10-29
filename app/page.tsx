8ca230c0735551cb43e347a3b3c80f8411d09e54
biplobsec21 <biplobsec21@gmail.com>
2023-10-29 12:41:07 +0600

    custome image added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './ui/home.module.css';
 
 import Link from 'next/link';
+import { lusitana } from './ui/font';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -13,9 +15,9 @@ export default function Page() {
           <div
             className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
           />
-          <div className={styles.shape}></div>
+          <div className={styles.shape}></div>;
 
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -30,7 +32,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
         </div>
       </div>
     </main>
